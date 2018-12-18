48ad6d758644369bae7c8ba6eb05b9db58b8fb4c
Jeff Langevin <jeff.langevin@gmail.com>
2018-12-17 22:21:56 -0500

    Updates

---
+++ precache-manifest.f9b22ad3468830742d2dc9c0de242fdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cad7cc190b859a78c4d5",
+    "url": "/tutorials-react-state-management/static/js/runtime~main.cad7cc19.js"
+  },
+  {
+    "revision": "c62686fde61fd741aa47",
+    "url": "/tutorials-react-state-management/static/js/main.c62686fd.chunk.js"
+  },
+  {
+    "revision": "3f80287cf7cf2478ac52",
+    "url": "/tutorials-react-state-management/static/js/1.3f80287c.chunk.js"
+  },
+  {
+    "revision": "c62686fde61fd741aa47",
+    "url": "/tutorials-react-state-management/static/css/main.1d5870fd.chunk.css"
+  },
+  {
+    "revision": "e1b0aaa295e3932c6786b8effbacf2f8",
+    "url": "/tutorials-react-state-management/index.html"
+  }
+];
